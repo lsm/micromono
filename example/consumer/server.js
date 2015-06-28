14dba6fccd693e3b6c18e329177081d282e48c15
Marc Liu <senmiao.liu@gmail.com>
2015-06-28 15:11:58 -0700

    Demonstrate the singleton factory class.

--- example/consumer/server.js
+++ example/consumer/server.js
@@ -6,7 +6,10 @@
 var micromono = require('micromono')();
 
 // require the service & probe it from network.
-micromono.require('provider');
+var Provider = micromono.require('provider');
+
+
+var p1 = new Provider();
 
 // Boot the service(s) do stuff in the promise callback.
 micromono.boot().then(function() {
@@ -16,6 +19,9 @@ micromono.boot().then(function() {
   // Get the provider instance.
   var provider = micromono.services.provider;
 
+  // Service class required from micromono is a singleton factory
+  var p2 = new Provider();
+  console.log(p1 === p2 && p2 === provider);
   // Call remote api as it is local.
   provider.createPost({
     title: 'A blog post',
