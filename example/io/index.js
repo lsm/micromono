38b48f5220d259d7bcf9f3bbed09bf054ccfda26
lsm <lsm@hac.local>
2015-09-10 23:45:34 -0700

    Allow set upgrade url in service definition.

--- example/io/index.js
+++ example/io/index.js
@@ -6,10 +6,18 @@ var Service = MicroMono.Service;
 
 var IO = module.exports = Service.extend({
   packagePath: __dirname,
+
+  // Base relative url for all routes and sockets
   baseUrl: '/io',
+  // Tell micromono to allow upgrade (websocket) connection for this path.
+  // The upgradeUrl is a relative path to the baseUrl of the service.
+  // The actuall path for client to connect will be: baseUrl + upgradeUrl,
+  // `/io/example-socket` in this case.
+  // Please see `public/io/main.js` for example.
+  upgradeUrl: '/example-socket',
 
   use: {
-    // tell micromono to use `layout` middleware at the server side
+    // Tell micromono to use `layout` middleware at the balancer side
     // for request url matching `/io/$`.
     'layout': '/$'
   },
@@ -21,15 +29,19 @@ var IO = module.exports = Service.extend({
   },
 
   init: function() {
-    var baseUrl = this.baseUrl;
-    var socketPath = '/example-socket';
+    // Calling `allowUpgrade` with no arguments will return
+    // the full url (baseUrl + upgradeUrl) which accepts upgrade request
+    var socketPath = this.allowUpgrade();
+    // You can override the above defined `upgradeUrl` by calling `allowUpgrade`
+    // with new url string you want
+    // this.allowUpgrade('/my/new-socket');
 
     // listen to the `server` event
     this.on('server', function(server) {
       console.log('Please open http://127.0.0.1:3000/io in your browser.');
       // setup socket.io with server
       var io = require('socket.io')(server, {
-        path: baseUrl + socketPath
+        path: socketPath
       });
 
       io.on('connection', function(socket) {
@@ -40,13 +52,6 @@ var IO = module.exports = Service.extend({
         });
       });
     });
-
-    // Tell micromono to allow websocket connection for this path.
-    // The socketPath is a relative path to the baseUrl of the service.
-    // The actuall path for client to connect will be: baseUrl + socketPath,
-    // `/io/example-socket` in this case.
-    // Please see `public/io/main.js` for example.
-    this.allowUpgrade(socketPath);
 
     // setup express app
     var app = this.app;
