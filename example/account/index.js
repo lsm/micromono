43e17ba0855aa427f622290ea3303667b8d46048
lsm <liusenmiao@gmail.com>
2016-01-05 23:14:47 -0800

    Remove semicolon.

--- example/account/index.js
+++ example/account/index.js
@@ -2,54 +2,54 @@
  * Module dependencies
  */
 
-var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var connect = require('connect');
+var path = require('path')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var connect = require('connect')
 
 // setup micromono
-var MicroMono = require('micromono');
-var Service = MicroMono.Service;
-var micromono = new MicroMono();
+var MicroMono = require('micromono')
+var Service = MicroMono.Service
+var micromono = new MicroMono()
 
 // get passport
-var passport = require('./passport');
+var passport = require('./passport')
 
 // generate passport authentication function
 var passportAuth = passport.authenticate('local', {
   successRedirect: '/account/protected',
   failureRedirect: '/account/login',
   failureFlash: false
-});
+})
 
 function isAuthenticated(req, res) {
   if (req.isAuthenticated()) {
-    return true;
+    return true
   } else {
-    res.redirect('/account/login');
-    return false;
+    res.redirect('/account/login')
+    return false
   }
 }
 
 // setup a dedicated connect middleware for parsing data and session,
 // so we can reuse it in the `auth` middleware and the express app.
-var connectAuth = connect();
+var connectAuth = connect()
 
-connectAuth.use(bodyParser.json());
+connectAuth.use(bodyParser.json())
 connectAuth.use(bodyParser.urlencoded({
   extended: false
-}));
+}))
 
-connectAuth.use(cookieParser());
+connectAuth.use(cookieParser())
 connectAuth.use(session({
   secret: 'micromono',
   resave: true,
   saveUninitialized: true
-}));
+}))
 
-connectAuth.use(passport.initialize());
-connectAuth.use(passport.session());
+connectAuth.use(passport.initialize())
+connectAuth.use(passport.session())
 
 /**
  * Account service
@@ -61,15 +61,15 @@ var Account = module.exports = Service.extend({
     auth: function() {
       return function(req, res, next) {
         if (req.isAuthenticated()) {
-          next();
+          next()
         } else {
           connectAuth(req, res, function() {
             if (isAuthenticated(req, res)) {
-              next();
+              next()
             }
-          });
+          })
         }
-      };
+      }
     }
   },
 
@@ -87,43 +87,43 @@ var Account = module.exports = Service.extend({
     /**
      * Example protected page
      */
-    'get::/protected': function protected(req, res) {
+    'get::/protected': function protectedPage(req, res) {
       if (isAuthenticated(req, res)) {
         res.render('hello', {
           name: req.user.username
-        });
+        })
       }
     },
 
     'post::^/logout': function logout(req, res) {
-      req.logout();
-      res.redirect('/account/login');
+      req.logout()
+      res.redirect('/account/login')
     },
 
     'get::/login': function login(req, res) {
-      res.render('login');
+      res.render('login')
     },
 
     /**
      * Login form handler
      */
     'post::/login': [passportAuth, function loginOkay(req, res) {
-      res.redirect('/account/protected');
+      res.redirect('/account/protected')
     }]
   },
 
   init: function() {
     // get express instance
-    var app = this.app;
+    var app = this.app
 
     // attach the connect auth middleware to our local express app
-    app.use(connectAuth);
+    app.use(connectAuth)
 
     // setup template engine
-    app.set('views', path.join(__dirname, './view'));
-    app.set('view engine', 'jade');
+    app.set('views', path.join(__dirname, './view'))
+    app.set('view engine', 'jade')
 
-    return Promise.resolve();
+    return Promise.resolve()
   },
 
   api: {
@@ -133,15 +133,15 @@ var Account = module.exports = Service.extend({
           id: 1,
           username: 'micromono',
           password: '123456'
-        });
+        })
       } else {
-        callback(null);
+        callback(null)
       }
     }
   }
-});
+})
 
 // Start the service if this is the main file
 if (require.main === module) {
-  micromono.startService(Account);
+  micromono.startService(Account)
 }
