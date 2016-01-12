f11f3a49e52820b054886689b2978f12d38a6e7a
lsm <liusenmiao@gmail.com>
2016-01-12 12:45:57 -0800

    Example for new `startBalancer` function.

--- example/server/server.js
+++ example/server/server.js
@@ -3,19 +3,19 @@
  */
 
 // setup express app
-var app = require('express')();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+var app = require('express')()
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
 
 // Get a micromono instance.
-var micromono = require('micromono')();
+var micromono = require('micromono')
 
 // Boot the service(s) with express server
 // do stuff in the promise callback.
-micromono.startBalancer(app).then(function() {
-  console.log('server booted');
+micromono.startBalancer(app, function(balancerAsset) {
+  console.log('server booted')
 
-  var assetInfo = micromono.asset.jspmInfo
+  var assetInfo = balancerAsset.jspmInfo
 
   if (assetInfo.urls.bundleJs) {
     app.locals.mainBundleJs = assetInfo.urls.bundleJs
@@ -28,5 +28,5 @@ micromono.startBalancer(app).then(function() {
     app.locals.mainEntryJs = assetInfo.main
   }
 
-  app.listen(3000);
-});
+  app.listen(3000)
+})
