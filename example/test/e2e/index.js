5fd5f598196b88ae1f831a1e2bc13687655790b1
lsm <liusenmiao@gmail.com>
2017-02-27 00:18:30 -0800

    [Fix] Add channel to e2e test & fix `make nats-cluster`.

--- example/test/e2e/index.js
+++ example/test/e2e/index.js
@@ -7,10 +7,17 @@ module.exports = {
 
     browser.expect.element('#title').text.to.equal('MicroMono Example').before(10000)
 
+    browser
+      .url(APP_URL + '/channel')
+
+    browser.expect.element('#sub-title').text.to.equal('MicroMono Channel Example').before(10000)
+    browser.pause(8000)
+
     // Teardown services so istanbul can generate reports
     browser.url(APP_URL + '/io/exit')
     browser.url(APP_URL + '/home/exit')
     browser.url(APP_URL + '/account/exit')
+    browser.url(APP_URL + '/channel/exit')
     browser.url(APP_URL + '/balancer/exit')
   }
 }
